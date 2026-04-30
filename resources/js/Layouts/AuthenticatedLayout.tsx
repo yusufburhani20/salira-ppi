d6ee0c706c8aa41fe33d27194ee2d601fbc41385
Yusuf Burhani <yusuf@smkfadris.sch.id>
2026-04-30 11:30:42 +0700

    update

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -108,7 +108,7 @@ export default function Authenticated({
                     label: 'Rekapitulasi',
                     href: route('admin.reports.index'),
                     active: route().current('admin.reports.index') || route().current('admin.reports.attendance.*') || route().current('admin.reports.assessments.*'),
-                    show: isAdmin || isPimpinan || isTeacher,
+                    show: isAdmin || isPimpinan,
                     icon: (<svg className="w-5 h-5 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>),
                 },
                 {
